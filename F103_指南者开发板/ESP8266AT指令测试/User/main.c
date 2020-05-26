2ab2a80dfd72d9112e5de04a5307a7bec4827788
Yuhailong <2816850683@qq.com>
2020-05-26 17:32:23 +0800

    修改了数据转发使用的函数

--- F103_指南者开发板/ESP8266AT指令测试/User/main.c
+++ F103_指南者开发板/ESP8266AT指令测试/User/main.c
@@ -27,8 +27,6 @@
   */
 int main(void)
 {	
-	
-	uint16_t i;
   /*初始化USART 配置模式为 115200 8-N-1，中断接收*/
   USART_Config();
 	
@@ -36,29 +34,22 @@ int main(void)
 	/* 发送一个字符串 */
 	printf("欢迎使用野火STM32开发板\n\n");
 	printf("这是一个ESP8266AT指令测试实验\n\n");
-	printf("请发送“AT”测试ESP8266是否准备好\n\n");
-	printf("使用串口调试助手时记得加上换行回车\n\n");
+	printf("请使用串口调试助手发送\"AT+换行回车\"测试ESP8266是否准备好\n\n");
 	printf("更多AT指令请参考模块资料\n\n");
 	printf("以下是ESP8266上电初始化打印的信息\n\n");
   while(1)
 	{	
 		if(strUSART_Fram_Record .InfBit .FramFinishFlag == 1)  //如果接收到了串口调试助手的数据
 		{
-			for(i = 0;i < strUSART_Fram_Record .InfBit .FramLength; i++)
-			{
-				 USART_SendData( macESP8266_USARTx ,strUSART_Fram_Record .Data_RX_BUF[i]); //转发给ESP82636
-				 while(USART_GetFlagStatus(macESP8266_USARTx,USART_FLAG_TC)==RESET){}      //等待发送完成
-			}
+			strUSART_Fram_Record .Data_RX_BUF[strUSART_Fram_Record .InfBit .FramLength] = '\0';
+			Usart_SendString(macESP8266_USARTx ,strUSART_Fram_Record .Data_RX_BUF);      //数据从串口调试助手转发到ESP8266
 			strUSART_Fram_Record .InfBit .FramLength = 0;                                //接收数据长度置零
 			strUSART_Fram_Record .InfBit .FramFinishFlag = 0;                            //接收标志置零
 	  }
 		if(strEsp8266_Fram_Record .InfBit .FramFinishFlag)                             //如果接收到了ESP8266的数据
 		{                                                      
-			for(i = 0;i < strEsp8266_Fram_Record .InfBit .FramLength; i++)               
-			{
-				 USART_SendData( DEBUG_USARTx ,strEsp8266_Fram_Record .Data_RX_BUF[i]);    //转发给ESP82636
-				 while(USART_GetFlagStatus(DEBUG_USARTx,USART_FLAG_TC)==RESET){}
-			}
+			 strEsp8266_Fram_Record .Data_RX_BUF[strEsp8266_Fram_Record .InfBit .FramLength] = '\0';
+			 Usart_SendString(DEBUG_USARTx ,strEsp8266_Fram_Record .Data_RX_BUF);        //数据从ESP8266转发到串口调试助手
 			 strEsp8266_Fram_Record .InfBit .FramLength = 0;                             //接收数据长度置零
 			 strEsp8266_Fram_Record.InfBit.FramFinishFlag = 0;                           //接收标志置零
 		}
