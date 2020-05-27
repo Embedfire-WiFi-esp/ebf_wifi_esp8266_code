d354f7f18e6f30646f8f195504f93212548d277c
Yuhailong <2816850683@qq.com>
2020-05-27 14:53:37 +0800

    在例程2wifi透传基础上修改成向电脑网络调试助手发送温湿度数据

--- F103_指南者开发板/3-向电脑网络助手上传 DHT11 温湿度/User/Test/test.c
+++ F103_指南者开发板/3-向电脑网络助手上传 DHT11 温湿度/User/Test/test.c
@@ -5,12 +5,61 @@
 #include <string.h>  
 #include <stdbool.h>
 #include "bsp_dht11.h"
+#include "bsp_usart.h"
+#include "bsp_led.h" 
 
-
-
+#define LED_CMD_NUMBER   8
+char *ledCmd[8] = { "LED_RED","LED_GREEN","LED_BLUE","LED_YELLOW","LED_PURPLE","LED_CYAN","LED_WHITE","LED_RGBOFF" };
+                        
+                     
+/**
+  * @brief  获取网络调试助手和串口调试助手发来的信息
+  * @param  无
+  * @retval 无
+  */
 volatile uint8_t ucTcpClosedFlag = 0;
 
+char cStr [ 1500 ] = { 0 };
 
+void Get_ESP82666_Cmd( char * cmd)
+{
+	uint8_t i;
+	for(i = 0;i < LED_CMD_NUMBER; i++)
+	{
+     if(( bool ) strstr ( cmd, ledCmd[i] ))
+		 break;
+	}
+	switch(i)
+    {
+      case 0:
+        LED_RED;
+      break;
+      case 1:
+        LED_GREEN;
+      break;
+      case 2:
+        LED_BLUE;
+      break;
+      case 3:
+        LED_YELLOW;
+      break;
+      case 4:
+        LED_PURPLE;
+      break;
+      case 5:
+        LED_CYAN;
+      break;
+      case 6:
+        LED_WHITE;
+      break;
+      case 7:
+        LED_RGBOFF;
+      break;
+      default:
+        
+        break;      
+    }   
+}
 
 /**
   * @brief  ESP8266 （Sta Tcp Client）透传
@@ -20,11 +69,7 @@ volatile uint8_t ucTcpClosedFlag = 0;
 void ESP8266_StaTcpClient_UnvarnishTest ( void )
 {
 	uint8_t ucStatus;
-	
-	char cStr [ 100 ] = { 0 };
-
 	DHT11_Data_TypeDef DHT11_Data;
-	
 	
   printf ( "\r\n正在配置 ESP8266 ......\r\n" );
 
@@ -43,10 +88,11 @@ void ESP8266_StaTcpClient_UnvarnishTest ( void )
 	while ( ! ESP8266_UnvarnishSend () );
 	
 	printf ( "\r\n配置 ESP8266 完毕\r\n" );
-	
+	printf ( "\r\n开始透传......\r\n" );
 	
 	while ( 1 )
-	{		
+	{
+		
 		if ( DHT11_Read_TempAndHumidity ( & DHT11_Data ) == SUCCESS )       //读取 DHT11 温湿度信息
 			sprintf ( cStr, "\r\n读取DHT11成功!\r\n\r\n湿度为%d.%d ％RH ，温度为 %d.%d℃ \r\n", 
 								DHT11_Data .humi_int, DHT11_Data .humi_deci, DHT11_Data .temp_int, DHT11_Data.temp_deci );
@@ -59,7 +105,7 @@ void ESP8266_StaTcpClient_UnvarnishTest ( void )
 	
 		ESP8266_SendString ( ENABLE, cStr, 0, Single_ID_0 );               //发送 DHT11 温湿度信息到网络调试助手
 		
-		Delay_ms ( 1500 );
+		Delay_ms ( 2000 );
 		
 		if ( ucTcpClosedFlag )                                             //检测是否失去连接
 		{
@@ -83,10 +129,10 @@ void ESP8266_StaTcpClient_UnvarnishTest ( void )
 			while ( ! ESP8266_UnvarnishSend () );		
 			
 		}
-
 	}
 	
 		
 }
+
 
 
